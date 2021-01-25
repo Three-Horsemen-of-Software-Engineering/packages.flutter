8a0df57e45c7bba8fb169d89b19441e3637e3daf
Andrii Iliunin <orkay255@gmail.com>
2021-01-25 16:11:58 +0200

    Fix "ambiguous return type error"

--- packages/native_pdf_renderer/android/src/main/kotlin/io/scer/pdf/renderer/utils/Random.kt
+++ packages/native_pdf_renderer/android/src/main/kotlin/io/scer/pdf/renderer/utils/Random.kt
@@ -2,6 +2,6 @@ package io.scer.pdf.renderer.utils
 
 import java.util.UUID.randomUUID
 
-val randomID get() = randomUUID().toString()
+val randomID: String get() = randomUUID().toString()
 
-val randomFilename get() = randomID.replace("-".toRegex(), "")
+val randomFilename: String get() = randomID.replace("-".toRegex(), "")
